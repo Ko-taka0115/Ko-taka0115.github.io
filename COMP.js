38ea2c5eccc69e7d79369d2b4eaead7e547c1bad
Ko-taka0115 <47974641+Ko-taka0115@users.noreply.github.com>
2019-03-30 09:35:51 +0900

    a

--- COMP.js
+++ COMP.js
@@ -5,3 +5,13 @@ time();
         document.getElementById("time").innerHTML = now.toLocaleTimeString();
     }
     setInterval('time()',1000);
+jQuery(function($) {
+        $.ajax({
+            url: "http://weather.livedoor.com/forecast/webservice/json/v1?city=340010",
+            dataType : 'jsonp',
+        }).done(function(data){
+            console.log(data);
+    }).fail(function(data) {
+            console.log("ERR");          
+  });
+});
